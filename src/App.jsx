7629b23d8ce2ef3fd694af8392dd1f7fd23d4a75
bbizin1004 <bbizin1004@korea.com>
2023-12-21 10:17:03 +0900

    feat: todo list 작업 시작

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import style from './App.module.css';
+
+export default function APP() {
+  return (
+    <div className={style.app}>
+      <h1>Todo List</h1>
+    </div>
+  );
+}
